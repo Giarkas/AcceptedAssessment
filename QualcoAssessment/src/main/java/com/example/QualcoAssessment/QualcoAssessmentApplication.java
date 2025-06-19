fec30348efb690936e105219d912d23eac1d1320
giarkas <giarkas@hotmail.com>
2025-06-19 20:25:05 +0300

    init

---
+++ QualcoAssessment/src/main/java/com/example/QualcoAssessment/QualcoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.QualcoAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualcoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualcoAssessmentApplication.class, args);
+	}
+
+}
---
+++ QualcoAssessment/src/test/java/com/example/QualcoAssessment/QualcoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QualcoAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualcoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
