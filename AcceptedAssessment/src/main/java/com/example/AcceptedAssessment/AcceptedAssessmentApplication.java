887fc6736c95fa14552b181345314bd885bbd329
arkadianos <giarkas@hotmail.com>
2025-07-07 13:20:25 +0300

    Create project

---
+++ AcceptedAssessment/src/main/java/com/example/AcceptedAssessment/AcceptedAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.AcceptedAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptedAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptedAssessmentApplication.class, args);
+	}
+
+}
---
+++ AcceptedAssessment/src/test/java/com/example/AcceptedAssessment/AcceptedAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AcceptedAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcceptedAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
